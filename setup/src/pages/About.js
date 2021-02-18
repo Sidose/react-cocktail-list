00610d66d280dcd900fec11fcf948fa582981498
Sergey Sydorenko <sidose4@gmail.com>
2021-02-18 22:43:03 +0200

    initial commit

---
+++ setup/src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+      standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+      a type specimen book.
+    </p>
+    </section>
+  )
+}
+
+export default About
